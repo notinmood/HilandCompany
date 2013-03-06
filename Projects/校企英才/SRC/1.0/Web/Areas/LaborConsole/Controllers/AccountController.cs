8bc431b18b1eb0c6c991459545d966c58fc23b6a
notinmood <notinmood@gmail.com>
2013-03-06 19:33:18 +0800

    完善个人登录部分的功能

--- Projects/校企英才/SRC/1.0/Web/Areas/LaborConsole/Controllers/AccountController.cs
+++ Projects/校企英才/SRC/1.0/Web/Areas/LaborConsole/Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HiLand.Framework.BusinessCore.BLL;
+using XQYC.Business.BLL;
+using XQYC.Business.Entity;
 
 namespace XQYC.Web.Areas.LaborConsole.Controllers
 {
@@ -31,6 +34,15 @@ namespace XQYC.Web.Areas.LaborConsole.Controllers
         /// </summary>
         /// <returns></returns>
         public ActionResult MyBasicInfo()
+        {
+            LaborEntity entity = LaborEntity.Empty;
+
+            entity = LaborBLL.Instance.Get(BusinessUserBLL.CurrentUserGuid);
+
+            return View(entity);
+        }
+
+        public ActionResult MyBasicInfoEdit()
         {
             return View();
         }
