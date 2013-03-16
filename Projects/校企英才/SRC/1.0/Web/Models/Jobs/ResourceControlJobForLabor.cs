55ef4f204a3c0841b0ec33d6360571ee1b557a6c
notinmood <notinmood@gmail.com>
2013-03-16 15:14:09 +0800

    劳务人员统计显示明细功能

--- Projects/校企英才/SRC/1.0/Web/Models/Jobs/ResourceControlJobForLabor.cs
+++ Projects/校企英才/SRC/1.0/Web/Models/Jobs/ResourceControlJobForLabor.cs
@@ -26,14 +26,17 @@ namespace XQYC.Web.Models.Jobs
             }
 
             //1. 释放录入及其修改后在一段时间内都未能形成派遣的劳务人员的保护
-            string sqlClauseForSignProtected = string.Format("update [XQYCLabor] set IsProtectedByOwner ={0} where LaborWorkStatus={1} AND CreateDate< '{2}' AND LastUpdateDate<'{3}' ",
-                (int)Logics.True, (int)LaborWorkStatuses.NewWorker, DateTime.Today.AddDays(-fromCreatedToSignedProtectedDays), DateTime.Today.AddDays(-fromUpdatedToSignedProtectedDays));
+            //string sqlClauseForSignProtected = string.Format("update [XQYCLabor] set IsProtectedByOwner ={0} where LaborWorkStatus={1} AND CreateDate< '{2}' AND LastUpdateDate<'{3}' ",
+            //    (int)Logics.False, (int)LaborWorkStatuses.NewWorker, DateTime.Today.AddDays(-fromCreatedToSignedProtectedDays), DateTime.Today.AddDays(-fromUpdatedToSignedProtectedDays));
 
-            int rowCountForCreateUpdateProtected = LaborBLL.Instance.ExcuteNonQuery(sqlClauseForSignProtected);
+            //int rowCountForCreateUpdateProtected = LaborBLL.Instance.ExcuteNonQuery(sqlClauseForSignProtected);
 
             //2.释放合同到期之后的一段时间内未能形成二次签约的劳务人员的保护
-            //string sqlClauseForReSignProtected = string.Format("update [XQYCLabor] set IsProtectedByOwner ={0} where LaborWorkStatus={1} AND CurrentContractStopDate!='{2}' AND CurrentContractStopDate< '{3}' AND LastUpdateDate<'{4}' ",
-            //    (int)Logics.True, (int)LaborWorkStatuses.NewWorker, DateTime.Today.AddDays(-fromCreatedToSignedProtectedDays), DateTime.Today.AddDays(fromUpdatedToSignedProtectedDays));
+            string sqlClauseForReSignProtected = string.Format("update [XQYCLabor] set IsProtectedByOwner ={0} where LaborWorkStatus !={1} AND CurrentContractDiscontinueDate<='{2}'  AND CurrentContractDiscontinueDate!='{3}' AND CurrentContractDiscontinueDate< '{4}' AND LastUpdateDate<'{5}' ",
+                (int)Logics.False, (int)LaborWorkStatuses.Worked, DateTime.Today.AddDays(-fromContractEndToResignedProtectedDays), DateTimeHelper.Min, DateTime.Today.AddDays(fromUpdatedToSignedProtectedDays));
+
+            LaborBLL.Instance.ExcuteNonQuery(sqlClauseForReSignProtected);
+            //3.释放****对劳务人员的保护
         }
 
         protected override string TaskNameInConfig
